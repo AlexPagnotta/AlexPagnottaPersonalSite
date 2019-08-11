451b408e4d1084e72e5c9ca308c14948209e3445
Alex Pagnotta <alex.pagnotta@outlook.it>
2019-08-11 11:44:39 +0200

    Initial work on navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./navbar.css"
+
+export default () => (
+  <nav class="navbar-container">
+    <div class="logo"></div>
+    <ul class="navbar-items">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+  </nav>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,10 @@
 import React from "react"
-import { Link, graphql  } from "gatsby"
 import Layout from "../components/layout"
+import Navbar from "../components/navbar"
 
+export default () => (
+  <Layout>
+    <Navbar></Navbar>
+  </Layout>
+)
 
-export default ({data}) => {
-  return (
-    <Layout>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <Link
-          to={node.fields.slug}>
-          <h1> {node.frontmatter.title} </h1>
-        </Link>
-      ))}
-    </Layout>
-  )
-}
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-  `
