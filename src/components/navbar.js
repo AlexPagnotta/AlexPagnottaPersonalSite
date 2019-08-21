14397cd97118072a6663f5efad79342dad24b1d6
Alex Pagnotta <alex.pagnotta@outlook.it>
2019-08-21 21:03:14 +0200

    Add styled components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,39 @@
 import React from "react"
-import "./navbar.css"
 import Img from "../components/image"
+import styled from "styled-components"
+
+const Container = styled.nav`
+height: 220px; 
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 30px;`
+
+const Logo = styled.div`
+width: 90px;
+height: 70px;
+background-color: white;`
+
+const MenuItems = styled.ul`
+list-style-type: none;
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+margin: 0px;`
+
+const MenuItem = styled.ul`
+margin-left: 30px;`
 
 export default () => (
-  <nav class="navbar-container">
-    <div class="logo">
+  <Container>
+    <Logo>
       <Img></Img>
-    </div>
-    <ul class="navbar-items">
-      <li>Home</li>
-      <li>Progetti</li>
-      <li>Chi Sono</li>
-      <li>Visita il mio Blog</li>
-    </ul>
-  </nav>
+    </Logo>
+    <MenuItems>
+      <MenuItem>Home</MenuItem>
+      <MenuItem>Progetti</MenuItem>
+      <MenuItem>Chi Sono</MenuItem>
+      <MenuItem>Visita il mio Blog</MenuItem>
+    </MenuItems>
+  </Container>
 )
