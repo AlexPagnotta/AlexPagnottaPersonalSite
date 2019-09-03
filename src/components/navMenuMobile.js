76fbddc8656b43c42166312b53892fd7fc92119b
Alex Pagnotta <alex.pagnotta@outlook.it>
2019-09-03 21:23:53 +0200

    Initial work on animation

--- src/components/navMenuMobile.js
+++ src/components/navMenuMobile.js
@@ -10,6 +10,38 @@ height:100%;
 width:100%;
 z-index:1;
 background-color: ${colors.primaryColor};
+-webkit-animation: slide-top 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+animation: slide-top 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+
+&.hidden {
+  visibility: hidden;
+  opacity(0);
+  -webkit-animation: slide-top 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+  animation: slide-top 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+}
+
+//Ease in Animation 
+@-webkit-keyframes slide-top {
+  0% {
+    -webkit-transform: translateY(-100vh);
+            transform: translateY(-100vh);
+  }
+  100% {
+    -webkit-transform: translateY(0);
+            transform: translateY(0);
+  }
+}
+@keyframes slide-top {
+  0% {
+    -webkit-transform: translateY(-100vh);
+            transform: translateY(-100vh);
+  }
+  100% {
+    -webkit-transform: translateY(0);
+            transform: translateY(0);
+  }
+}
+          
 `
 
 const MobileMenuItems = styled.ul`
@@ -46,8 +78,7 @@ function NavMenuMobile() {
             <MobileMenuItem>Visita il mio Blog</MobileMenuItem>
         </MobileMenuItems>
     </OverlayContainer>
-    
+ 
   )
-
 }
 export default NavMenuMobile
