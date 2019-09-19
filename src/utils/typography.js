266e575eab475541dca83c5cb6362b5c5bf4dc51
Alex Pagnotta <alex.pagnotta@outlook.it>
2019-09-19 21:03:22 +0200

    Change to typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,9 @@
 import Typography from "typography"
 
 const typography = new Typography({
-  baseFontSize: "25px",
+  baseFontSize: "20px",
   baseLineHeight: 1.666,
-  scaleRatio: 2,
+  scaleRatio: 2.5,
   headerFontFamily: ["Lato", "900"],
   bodyFontFamily: ["Lato", "400"],
   googleFonts: [
