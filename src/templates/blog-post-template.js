adaaf6b138d369839640c84a30af345a7b8bf05b
Alex Pagnotta <alex.pagnotta@outlook.it>
2019-12-29 13:48:05 +0100

    Blog Post Template UI Changes

--- src/templates/blog-post-template.js
+++ src/templates/blog-post-template.js
@@ -15,7 +15,34 @@ margin: 0 auto; //Center the container
 ${MediaQueries.queries.tablet`
 padding-top: 100px; 
 `}
+`
 
+const TitleContainer = styled.div`
+padding: 30px 30px 0px 30px;
+margin-top: 60px;
+width: 100%;
+text-align: center
+`
+
+const SubTitleContainer = styled.div`
+padding: 0px;
+margin-top: 0px;
+width: 100%;
+text-align: center
+`
+
+const TextContainer = styled.div`
+padding: 30px 30px 30px 30px;
+
+& > div :first-child:first-letter {
+  float: left;
+  font-family: Bitter;
+  font-size: 75px;
+  line-height: 60px;
+  padding-top: 4px;
+  padding-right: 8px;
+  padding-left: 3px;
+}
 `
 
 export default ({ data }) => {
@@ -24,8 +51,15 @@ export default ({ data }) => {
     <Layout>
       <NavbarBlog ></NavbarBlog>
       <BlogPostContainer>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <TitleContainer>
+          <h1>{post.frontmatter.title}</h1>        
+        </TitleContainer>
+        <SubTitleContainer>
+          <h4>{post.frontmatter.date}</h4>        
+        </SubTitleContainer>
+        <TextContainer>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />       
+        </TextContainer>
       </BlogPostContainer>
     </Layout>
   )
@@ -37,6 +71,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date(formatString: "DD MMMM, YYYY", locale: "it")
       }
     }
   }
